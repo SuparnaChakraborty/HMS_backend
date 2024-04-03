a9e64302b98832523b86ad877cba66a0e4e7564b
Suparna Chakraborty <109012314+SuparnaChakraborty@users.noreply.github.com>
2024-04-03 20:06:04 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,19 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyCfNhbQ76jm7d0DLkdzGsUMTZhqz1JhYdI",
+  authDomain: "medcare-14062.firebaseapp.com",
+  projectId: "medcare-14062",
+  storageBucket: "medcare-14062.appspot.com",
+  messagingSenderId: "706528329777",
+  appId: "1:706528329777:web:64b716aeab96ba889d9510"
 };
 
+// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-export const storage = getStorage(firebaseApp);
+ export const storage = getStorage(firebaseApp);
